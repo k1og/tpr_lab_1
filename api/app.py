3ab5c17fa67bfc8c3f778b64ba13643fad1ea157
Alexey <headofhell@gmail.com>
2020-02-16 17:05:52 +0400

    implement maximax model api & small refactor

--- api/app.py
+++ api/app.py
@@ -12,9 +12,26 @@ def wald():
         abort(400)
     raw_matrix = request.json['matrix']
     matrix = np.array(raw_matrix)
+    print(matrix)
     try: 
-        number_station = matrix.min(axis=1).argmax() + 1
-        crit = matrix.min(axis=1).max()
+        raw_of_mins = matrix.min(axis=1)
+        number_station = raw_of_mins.argmax() + 1
+        crit = raw_of_mins.max()
+    except:
+        abort(422)
+    return 'Станция {}. Критерий = {}'.format(number_station, crit)
+
+# Критерий азартного игрока
+@app.route('/classic/maximax', methods=['POST'])
+def maximax():
+    if not request.json or not 'matrix' in request.json:
+        abort(400)
+    raw_matrix = request.json['matrix']
+    matrix = np.array(raw_matrix)
+    try:
+        raw_of_maxs = matrix.max(axis=1)
+        number_station = raw_of_maxs.argmax() + 1
+        crit = raw_of_maxs.max()
     except:
         abort(422)
     return 'Станция {}. Критерий = {}'.format(number_station, crit)
