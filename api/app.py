ae3ec2ddeb070c725d7fb26984694f70b533be45
Alexey <headofhell@gmail.com>
2020-02-16 16:53:03 +0400

    implement walds maximun model api

--- api/app.py
+++ api/app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, request, abort, Response
+# from flask_cors import CORS
+import numpy as np
 
 app = Flask(__name__)
+# CORS(app)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Wald's maximin model
+@app.route('/classic/wald', methods=['POST'])
+def wald():
+    if not request.json or not 'matrix' in request.json:
+        abort(400)
+    raw_matrix = request.json['matrix']
+    matrix = np.array(raw_matrix)
+    try: 
+        number_station = matrix.min(axis=1).argmax() + 1
+        crit = matrix.min(axis=1).max()
+    except:
+        abort(422)
+    return 'Станция {}. Критерий = {}'.format(number_station, crit)
